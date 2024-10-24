fcae36d5dc11291e86d7670948c8534380c1a4c7
Phamdaaa <francocioffi751@gmail.com>
2024-10-24 08:00:14 -0300

    24/10

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import Hero from './componentes/Hero';
 import Skills from './componentes/Skills';
 import Projects from './componentes/Projects';
 import Contact from './componentes/Contact';
-import Parse from 'parse/dist/parse.min.js';
-const PARSE_APPLICATION_ID = 'k0YhGSDi7eL1PY9lohuXxrxSCaeAuuxQjRMsBqmK';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'VeYTFOrowkHTLHPDDmkCggx3pZuxcfZD8aOvjciu';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+
+
 function App() {
   return (
     <div>
